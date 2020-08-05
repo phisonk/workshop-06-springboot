ae12d115ab4fef575bc77a711c9c211697b5a809
phisonkha <phison.khankhang@allianz.com>
2020-08-05 15:52:17 +0700

    get user from id by repository

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -36,7 +37,8 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public UsersResponse getUserById(@PathVariable int id){
-        return new UsersResponse(id,"User "+id);
+        Optional<User> user =  userRepository.findById(id);
+        return new UsersResponse(user.get().getId(),user.get().getName());
     }
 
     @PostMapping(path = "/users")
