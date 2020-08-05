0bb3894470d8431d2b4603901bc2ffd50bcad1f1
phisonkha <phison.khankhang@allianz.com>
2020-08-05 16:07:11 +0700

    get all return id name age

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -38,7 +38,7 @@ public class UserController {
     @GetMapping("/users/{id}")
     public UsersResponse getUserById(@PathVariable int id){
         Optional<User> user =  userRepository.findById(id);
-        return new UsersResponse(user.get().getId(),user.get().getName());
+        return new UsersResponse(user.get().getId(),user.get().getName(),user.get().getAge());
     }
 
     @PostMapping(path = "/users")
--- src/main/java/com/example/demo/UsersResponse.java
+++ src/main/java/com/example/demo/UsersResponse.java
@@ -3,13 +3,20 @@ package com.example.demo;
 public class UsersResponse {
     private int id;
     private String name;
+    private int age;
 
     // POJO (Plain Old Java Object)
     // Getter/Setter methods
     public UsersResponse(int id, String name) {
+        this(id,name,0);
+    }
+
+    public UsersResponse(int id, String name, int age) {
         this.id = id;
         this.name = name;
+        this.age = age;
     }
+
     public int getId() {
         return id;
     }
